758e7f37a54167dab23a7fb0f981b725c15be3aa
Guilherme Pardi <guilhermepardi@gmail.com>
2023-08-21 20:36:06 -0300

    feat: Estrutura da página inicial Register

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,82 @@
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { Container, Form, Header } from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormError } from '../home/components/ClaimUsernameForm/styles'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O nome de usuário deve ter pelo menos 3 caracteres' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário precisa ter apenas letras e números',
+    })
+    .transform((username) => username.toLowerCase()),
+
+  name: z
+    .string()
+    .min(4, { message: 'O nome precisa ter mais de 4 caracteres' })
+    .regex(/^([a-zA-Z\s]+)$/, {
+      message: 'O nome deve ser composto apenas por letras',
+    })
+    .transform((name) => name.toLowerCase()),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  async function handleRegister(data: RegisterFormData) {
+    console.log(data)
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading>Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1}></MultiStep>
+      </Header>
+      <Form as={'form'} onSubmit={handleSubmit(handleRegister)}>
+        <label>
+          <Text size={'sm'}>Nome de usuário</Text>
+          <TextInput
+            crossOrigin={undefined}
+            prefix="cal.com/"
+            placeholder="seu-usuario"
+            {...register('username')}
+          />
+          {errors.username && (
+            <FormError size="sm">{errors.username.message}</FormError>
+          )}
+        </label>
+        <label>
+          <Text size={'sm'}>Nome completo</Text>
+          <TextInput
+            crossOrigin={undefined}
+            placeholder="Nome completo"
+            {...register('name')}
+          />
+          {errors.name && (
+            <FormError size="sm">{errors.name.message}</FormError>
+          )}
+        </label>
+        <Button type="submit">
+          Próximo passo <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
