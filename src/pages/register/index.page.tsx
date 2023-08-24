a52548afd9be20d8f9732c9ad1fcfc25e68a669a
Guilherme Pardi <guilhermepardi@gmail.com>
2023-08-23 21:58:23 -0300

    feat: adding check to validate if the user already exists

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -8,6 +8,7 @@ import { FormError } from '../home/components/ClaimUsernameForm/styles'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { api } from '@/src/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -54,6 +55,11 @@ export default function Register() {
         username: data.username,
       })
     } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err?.response?.data?.message)
+        return
+      }
+
       console.log(err)
     }
   }
