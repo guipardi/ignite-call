e51c7a64e44c1d2f9e12d135551b6f9e0e14a0be
Guilherme Pardi <guilhermepardi@gmail.com>
2023-09-13 20:32:06 -0300

    feat: adding structure lista de horarios

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,45 @@
 import { Calendar } from '@/src/components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            Terça-Feira, <span>22 de outubro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+            <TimePickerItem>9:00</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
