b81cd8ff98a1a29aa72d38851e36fd8162b4ee50
Guilherme Pardi <guilhermepardi@gmail.com>
2023-09-14 21:29:09 -0300

    feat: adding Confirm Step Page

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,46 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { CalendarBlank, Clock } from 'phosphor-react'
+
+export function ConfirmStep() {
+  return (
+    <ConfirmForm as="form">
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18 horas
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" crossOrigin={undefined} />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de email</Text>
+        <TextInput
+          type="email"
+          placeholder="guilhermepardi@gmail.com"
+          crossOrigin={undefined}
+        />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
