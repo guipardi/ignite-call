cd5a3a87441bce7a390c0c7f69babb2e6ec37b84
Guilherme Pardi <guilhermepardi@gmail.com>
2023-09-15 21:59:33 -0300

    feat: disabling the days that have already passed in the month and creating a mechanism that when you click on a day opens the available times screen (we haven't transferred the information to the times screen yet, so it's still static)

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -21,7 +21,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[]
 
-export function Calendar() {
+interface CalendarProps {
+  selectedDate?: Date | null
+  onDateSelected: (data: Date) => void
+}
+
+export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1)
   })
@@ -81,7 +86,7 @@ export function Calendar() {
         return { date, disabled: true }
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: false }
+        return { date, disabled: date.endOf('day').isBefore(new Date()) }
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true }
@@ -139,7 +144,10 @@ export function Calendar() {
                 {week.days.map((day) => {
                   return (
                     <td key={day.date.toString()}>
-                      <CalendarDay disabled={day.disabled}>
+                      <CalendarDay
+                        disabled={day.disabled}
+                        onClick={() => onDateSelected(day.date.toDate())}
+                      >
                         {day.date.get('date')}
                       </CalendarDay>
                     </td>
